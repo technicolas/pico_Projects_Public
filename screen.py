8a378767dd30bc2e8587e69c555d90879674504e
Nicolas <technicolas@gmail.com>
2022-11-12 01:04:58 +0100

    Update screen.py

--- screen.py
+++ screen.py
@@ -8,6 +8,9 @@ from oled import Write, GFX, SSD1306_I2C
 from oled.fonts import ubuntu_mono_15, ubuntu_mono_20
 import utime
 
+sensor_temp = machine.ADC(4)
+conversion_factor = 3.3 / (65535)
+
 led_interne = Pin(25, Pin.OUT)
 led_rouge = Pin(15, Pin.OUT)
 led_jaune = Pin(14, Pin.OUT)
@@ -22,15 +25,13 @@ write15 = Write(oled, ubuntu_mono_15)
 write20 = Write(oled, ubuntu_mono_20)
 
 def afficheur(text):
-#     oled.fill(0)
-#     write20.text("LED", 0, 0)
-#     write15.text("Allumee:", 20, 20)
-#     oled.text(text, 20, 40)
-#     oled.show()
     oled.fill(0)
-    write20.text("LED", 20, 0)
-    write15.text("ROUGE|ORANGE|VERTE", 0, 20)
-    oled.text(text, 0, 40)
+    write20.text("LED:", 0, 0)
+#    write15.text("ROUGE|ORANGE|VERTE", 0, 20)
+    oled.text(text, 45,10)
+    oled.text("t:",0,25)
+    oled.text(str(t),15,25)
+    oled.text("C",83,25)
     oled.show()
 
 def cligno(t):
@@ -62,12 +63,14 @@ def cligno3(t):
     utime.sleep(t)
 
 while True:
-    afficheur("  X")
+    reading = sensor_temp.read_u16() * conversion_factor
+    t= 27 - (reading - 0.706)/0.001721
+    afficheur("R |   |")
     cligno2(temps,1,0,0)
-    afficheur("       X")
+    afficheur("  | O |")
     cligno2(temps*1.5,0,1,0)
-    afficheur("             X")
+    afficheur("  |   | V")
     cligno2(temps,0,0,1)
-    afficheur("       X")
+    afficheur("  | O |")
     cligno2(temps*1.2,0,1,0)
 #     cligno3(1)
